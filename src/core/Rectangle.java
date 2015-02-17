78241f00361d171e794ed3ac2bb8dbe51b0a2eaa
asangaw <asangasw@gmail.com>
2015-02-17 17:46:52 +1100

    commiting rectangle class file

---
+++ src/core/Rectangle.java
@@ -0,0 +1,11 @@
+package core;
+
+/**
+ * Created by asangaw on 2/17/2015.
+ */
+public class Rectangle implements Shape {
+    @Override
+    public double calculateArea() {
+        return 0;
+    }
+}
